ad084cb1c2503d1c0e5f538f99ae62a5a36acf03
AadityaAgni <aadityaagni@gmail.com>
2024-01-06 11:33:19 -0700

    Moved Swerve Stuff Over

---
+++ src/main/java/frc/robot/constants/CompConstants.java
@@ -0,0 +1,17 @@
+package frc.robot.constants;
+
+/**
+ * CompConstants
+ */
+public class CompConstants {
+    private CompConstants() {
+        throw new IllegalStateException("Constants Class");
+    }
+
+    public static final boolean DEBUG_MODE = true;
+
+    // Not Set Up
+    public static final boolean PID_TUNE_MODE = false;
+
+
+}
