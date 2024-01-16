df165554dce3fc9b1891261f5672712944bd641a
Teja Koripella <tejakoripella@gmail.com>
2024-01-15 19:58:58 -0800

    Typo

--- src/main/java/frc/robot/commands/FollowPath.java
+++ src/main/java/frc/robot/commands/FollowPath.java
@@ -11,7 +11,7 @@ import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
 
-import edu.wpi.first.math.geometry.d;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
