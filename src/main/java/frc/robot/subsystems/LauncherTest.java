0dc4302792327f0d1b7dd1c22f0bfdba0c0b733d
SatishVaibhav <s-vsatish@lwsd.org>
2024-01-20 14:53:24 -0800

    Updates

--- src/main/java/frc/robot/subsystems/LauncherTest.java
+++ src/main/java/frc/robot/subsystems/LauncherTest.java
@@ -8,16 +8,17 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LauncherConstants;
+import frc.robot.subsystems.Launcher.LauncherMode;
 
 public class LauncherTest extends SubsystemBase{
 
 private double amp_length = 1.5; 
-private double amp_width = 2; 
+//private double amp_width = 2; 
 private double speaker_length = 6.5; 
-private double speaker_width = 3;
+//private double speaker_width = 3;
 private double robot_pos; 
 private double robot_shoot_angle;
-private boolean is_amp = true; 
+private LauncherMode mLauncherMode = LauncherMode.SPEAKER;
 
     public LauncherTest(double pos, double angle){
         robot_pos = pos; 
@@ -25,11 +26,15 @@ private boolean is_amp = true;
     }
 
     public double calculate_distance() {
-        if (is_amp){
+        if (mLauncherMode != LauncherMode.SPEAKER){
         return Math.sqrt((amp_length * amp_length) + (robot_pos * robot_pos)); 
         }
     return Math.sqrt((speaker_length * speaker_length) + (robot_pos * robot_pos)); 
     }
-
     
+    public String test_shot() {
+      return null; 
+
+    }
+
 } 
