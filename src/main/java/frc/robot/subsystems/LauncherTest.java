3d689f0e0dd20eff6c38308228db9687bc7cf220
SatishVaibhav <s-vsatish@lwsd.org>
2024-01-26 20:02:16 -0800

    Updated Launcher test with a mode test

--- src/main/java/frc/robot/subsystems/LauncherTest.java
+++ src/main/java/frc/robot/subsystems/LauncherTest.java
@@ -1,15 +1,9 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.LauncherConstants;
 import frc.robot.constants.LauncherConstants.LauncherMode;
 import frc.robot.constants.LauncherConstants.LauncherState;
-
+import frc.robot.subsystems.Launcher; 
 public class LauncherTest extends SubsystemBase{
 
 private double amp_length = 1.5; 
@@ -18,12 +12,29 @@ private double speaker_length = 6.5;
 //private double speaker_width = 3;
 private double robot_pos; 
 private double robot_shoot_angle;
-private LauncherMode mLauncherMode = LauncherMode.SPEAKER;
-
-    public LauncherTest(double pos, double angle){
+private LauncherMode mLauncherMode;
+private double mvelocity; 
+    public LauncherTest(double pos, double angle, LauncherMode LauncherMode, double velocity){
         robot_pos = pos; 
         robot_shoot_angle = angle; 
+        mLauncherMode = LauncherMode; 
+        mvelocity = velocity; 
     }
+
+
+    public Boolean launcher_mode_test(){
+        if (mLauncherMode == LauncherMode.OFF){
+            if (mvelocity > 0){
+                return false; 
+            }else{ 
+                return true; 
+            }
+        }else if(mLauncherMode == LauncherMode.AMP && mvelocity == 0 || mLauncherMode == LauncherMode.SPEAKER && mvelocity == 0|| mLauncherMode == LauncherMode.TRAP && mvelocity == 0){
+            return false; 
+        }
+        return true; 
+    }
+
 
     public double calculate_distance() {
         if (mLauncherMode != LauncherMode.SPEAKER){
@@ -32,6 +43,7 @@ private LauncherMode mLauncherMode = LauncherMode.SPEAKER;
     return Math.sqrt((speaker_length * speaker_length) + (robot_pos * robot_pos)); 
     }
     
+
     public String test_shot() {
       return null; 
 
