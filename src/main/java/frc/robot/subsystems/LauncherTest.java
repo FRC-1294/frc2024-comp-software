863a7608afdcc4776b33e4c33eec431c845a1aa3
SatishVaibhav <s-vsatish@lwsd.org>
2024-01-26 19:22:21 -0800

    Working on LauncherTest.java

--- src/main/java/frc/robot/subsystems/LauncherTest.java
+++ src/main/java/frc/robot/subsystems/LauncherTest.java
@@ -1,1 +1,40 @@
+package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.LauncherConstants;
+import frc.robot.constants.LauncherConstants.LauncherMode;
+import frc.robot.constants.LauncherConstants.LauncherState;
+
+public class LauncherTest extends SubsystemBase{
+
+private double amp_length = 1.5; 
+//private double amp_width = 2; 
+private double speaker_length = 6.5; 
+//private double speaker_width = 3;
+private double robot_pos; 
+private double robot_shoot_angle;
+private LauncherMode mLauncherMode = LauncherMode.SPEAKER;
+
+    public LauncherTest(double pos, double angle){
+        robot_pos = pos; 
+        robot_shoot_angle = angle; 
+    }
+
+    public double calculate_distance() {
+        if (mLauncherMode != LauncherMode.SPEAKER){
+        return Math.sqrt((amp_length * amp_length) + (robot_pos * robot_pos)); 
+        }
+    return Math.sqrt((speaker_length * speaker_length) + (robot_pos * robot_pos)); 
+    }
+    
+    public String test_shot() {
+      return null; 
+
+    }
+
+} 
