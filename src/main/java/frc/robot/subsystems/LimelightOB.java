8250894b95230ed724c988f045aecc5ba44ae1fd
Powerlax <raovishruth@gmail.com>
2024-02-09 19:55:13 -0800

    LimelightOB Working Now lol

--- src/main/java/frc/robot/subsystems/LimelightOB.java
+++ src/main/java/frc/robot/subsystems/LimelightOB.java
@@ -5,14 +5,44 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightOB extends SubsystemBase {
   NetworkTable mTable;
-  public LimelightOB() {}
+  String mLimeLightName;
+  double tv;
+  double tx;
+  double ta;
+  public LimelightOB() {
+    mLimeLightName = "limelight";
+    mTable = NetworkTableInstance.getDefault().getTable(mLimeLightName);
+  }
+  public LimelightOB(String LimeLightName) {
+    mLimeLightName = LimeLightName;
+    mTable = NetworkTableInstance.getDefault().getTable(mLimeLightName);
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    tv = mTable.getEntry("tv").getDouble(0);
+    ta = mTable.getEntry("ta").getDouble(0);
+    tx = mTable.getEntry("tx").getDouble(0);
+  }
+
+  public boolean isDetectionValid() {
+    return (tv == 1.0 && ta >= 2.0);
+  }
+
+  public double getTX() {
+    return tx;
+  }
+
+  public double getTA() {
+    return ta;
+  }
+
+  public double getTV() {
+    return tv;
   }
 }
