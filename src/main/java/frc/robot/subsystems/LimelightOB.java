48fef6b2d26c6d07b16a4b0ec6bc2c01cb50b456
SatishVaibhav <s-vsatish@lwsd.org>
2024-02-09 19:28:46 -0800

    Updates

--- src/main/java/frc/robot/subsystems/LimelightOB.java
+++ src/main/java/frc/robot/subsystems/LimelightOB.java
@@ -5,14 +5,35 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.IntakeConstants;
+import frc.robot.subsystems.Input;
+import frc.robot.subsystems.IntakeSubsystem; 
+import frc.robot.subsystems.SwerveSubsystem; 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class LimelightOB extends SubsystemBase {
   NetworkTable mTable;
+  IntakeSubsystem m_intake; 
   public LimelightOB() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public Command getAutomousIntakeCommand() {
+    return new FunctionalCommand(() -> m_intake.intakeAtSpeed(IntakeConstants.INTAKE_SPEED), null, interrupted -> m_intake.stopMotor(), this::functionalCommandIsFinished, this);    
+  }
+
+  public 
+
+  private boolean functionalCommandIsFinished() {
+    return m_intake.pieceInIntake() || Input.getLeftBumper();
   }
 }
