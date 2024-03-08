c8b6818272009c02d2d72468e0db29e0b5031616
Powerlax <raovishruth@gmail.com>
2024-03-07 22:44:02 -0800

    fixed intake constants

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -9,14 +9,18 @@ public class IntakeConstants {
     private IntakeConstants() {
         throw new IllegalStateException("Constants Class");
     }
-    public static final int INTAKE_SPARK_ID_INNER = 26;
-    public static final boolean INTAKE_INVERTED_INNER = true;
-    public static final int INTAKE_SPARK_ID_OUTER = 34; // will change later https://github.com/FRC-1294/frc2024/issues/239
+    public static final int INTAKE_SPARK_ID_INNER = 30; //Done
+    public static final boolean INTAKE_INVERTED_INNER = false;
+    public static final int INTAKE_SPARK_ID_OUTER = 36; //Done
     public static final boolean INTAKE_INVERTED_OUTER = true; // will change later https://github.com/FRC-1294/frc2024/issues/239
-    public static final int SMART_CURRENT_LIMIT_INNER = 60;
-    public static final int SMART_CURRENT_LIMIT_OUTER = 60;
-    public static final int INTAKE_BEAMBREAK_ID = 0;
-    public static final double ACTIVE_INTAKE_SPEED = 1.0;
+    public static final int SMART_CURRENT_LIMIT_INNER = 0; // will change later https://github.com/FRC-1294/frc2024/issues/239
+    public static final int SMART_CURRENT_LIMIT_OUTER = 0; // will change later https://github.com/FRC-1294/frc2024/issues/239
+    public static final int INTAKE_BEAMBREAK_ID = 1;
+    public static final double ACTIVE_INTAKE_SPEED = 0.6;
     public static final double PASSIVE_INTAKE_SPEED = 0;
+
+    public static final double HANDOFF_SPEED = 0.3;
+
+    public static final double NOMINAL_VOLTAGE = 11;
 
 }
